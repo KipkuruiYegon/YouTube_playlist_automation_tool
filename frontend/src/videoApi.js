aad9bac8c13fe8754c4a8460dfb7b66843348aea
KipkuruiYegon <kevinkipkurui1@gmail.com>
2025-03-09 17:06:11 +0300

    videoApi file utility

---
+++ frontend/src/videoApi.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const fetchVideos = async () => {
+    try {
+        const response = await axios.get('http://localhost:8000/api/ungrouped-videos');
+        return response.data; // Return the data for use elsewhere
+    } catch (error) {
+        console.error('Error fetching videos:', error);
+        throw error; // Re-throw the error for further handling
+    }
+};
+
+export default fetchVideos;
