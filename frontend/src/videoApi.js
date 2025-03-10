a945fd2d1ade07fa15ecf2779f96ced9cc7adc52
KipkuruiYegon <89918609+KipkuruiYegon@users.noreply.github.com>
2025-03-10 23:55:03 +0300

    Update videoApi.js

--- frontend/src/videoApi.js
+++ frontend/src/videoApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const fetchVideos = async () => {
     try {
-        const response = await axios.get('http://localhost:8000/api/ungrouped');
+        const response = await axios.get('http://localhost:8000/api/ungrouped-videos');
         return response.data; // Return the data for use elsewhere
     } catch (error) {
         console.error('Error fetching videos:', error);
